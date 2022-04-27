a4b575ae5a38725ca3ecaa1db7be797429faea8d
Richard <richard@formassembly.com>
2022-04-27 14:44:49 -0400

    TP-5 Updated settings

--- variables.php
+++ variables.php
@@ -1,8 +1,8 @@
  <?php
       $count = 0;     // Set count equal to 0
-      $_xval = 5;
-      $_yval = 1.0;
-      $some_string = "Hello There!";
-      $will i work=6; // This does not work!!!
+      $_xval = 6;
+      $_yval = 2.0;
+      $some_string = "Hello There You!";
+      $will iwork=6; // This does not work!!!
       $3blindmice=3;  // Same here, does not work
  ?>
